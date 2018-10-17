0c1a5e9a6168f818080f1f59464d20f2d1983e2a
Tihomir <tihomir_alcudia3@hotmail.com>
2018-10-17 20:36:45 +0200

    minimo terminado

--- papito/models/models.py
+++ papito/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.fields import One2many, Many2many
 
 
 class city(models.Model):
@@ -9,29 +10,36 @@ class city(models.Model):
     description = fields.Text()
     ubication = fields.Char(String="Ubication")
     country = fields.Many2one("res.country", "Pais")
-    hotelList = fields.One2many("papito.hotel","Hotel")
+    hotellist = fields.Many2many("papito.hotel")
 
     class hotel(models.Model):
         _name = 'papito.hotel'
         name = fields.Char()
-        galeriaFotos = fields.Binary()
+        galeriaFotos = Many2many("papito.fotos")
         description=fields.Text()
-        listaHabitaciones=fields.Many2one("papito.habitacion","Lista Habitaciones")
+        roomlist=fields.Many2many("papito.habitacion")
         valoraciones=fields.Integer()
-        listaServicios=fields.Many2one("papito.reserva","Lista Reservas")
+        listaServicios=fields.Many2many("papito.reserva")
 
     class habitacion(models.Model):
         _name = 'papito.habitacion'
-        hotel = fields.Many2one("papito.hotel", "Hotel")
+        hotel = fields.Many2many("papito.hotel", "Hotel")
         name = fields.Char()
-        camas = fields.Text()
-        fotos=fields.Text()
+        camas = fields.Integer()
+        fotos=Many2many("papito.fotos")
         precios=fields.Integer()
         descripcion=fields.Text()
 
     class reserva(models.Model):
         _name = 'papito.reserva'
-        fe  cha=fields.Date()
+        fechaInicio=fields.Date()
+        fechaFinal = fields.Date()
         habitaciones=fields.Many2one("papito.habitacion", "Habitacion a reservar")
+
+    class fotos(models.Model):
+        _name = 'papito.fotos'
+        name=fields.Char("Nombre de la foto")
+        foto=fields.Binary("Seleccione una foto para agregar")
+
 
 
